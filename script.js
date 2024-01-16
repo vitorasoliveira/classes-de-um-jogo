8fe478fcc77080b38629ae52911e9672e7b2496e
Vitor Alex <vitoroliveirace@gmail.com>
2024-01-16 20:19:20 -0300

    "Initial commit"

---
+++ script.js
@@ -0,0 +1,24 @@
+class FormaDeHero {
+    constructor(name, age, type) {
+        this.name = name
+        this.age = age
+        this.type = type
+    }
+
+    atack() {
+        if (this.type === "mago") {
+            console.log(`O ${this.type} atacou uasando magia`)
+        } else if (this.type === "guerreiro") {
+            console.log(`O ${this.type} atacou uasando espada`)
+        } else if (this.type === "monge") {
+            console.log(`O ${this.type} atacou uasando artes marciais`)
+        } else if (this.type === "ninja") {
+            console.log(`O ${this.type} atacou uasando shuriken`)
+        } else {
+            console.log("Sorry princess, there's no such thing around here! Type it again, would you..?")
+        }
+    }
+}
+
+let felipaoDaHero = new FormaDeHero("Felipão", 72, "ninja")
+felipaoDaHero.atack()
